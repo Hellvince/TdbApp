7b063adec018c5a64501c8c24cd4694e69f71cca
Vincent Canuel <vincentcanuel@gmail.com>
2014-08-25 10:56:00 +0200

    Cleaning global.R

--- global.R
+++ global.R
@@ -1,23 +1,27 @@
+################################################################################
+##
+##      Title   : slobal.R (part of the Shiny application called TdbApp)
+##      Author  : Vincent Canuel
+##      Date    :
+##
+##
+################################################################################
+
+################################################################################
+##
+##      Main parameters
+##
+################################################################################
+
+# List of supported cancer type
+localisation <- c("sein")
+
+
+# Loading required libraries
 library(plyr)
 library(ggplot2)
 
-# Load the SAS outputs and merge them
-# Create a single dataframe for each cancer localisation
-loadData <- function(x) {
-    filenames <- list.files(paste0("data/",x), pattern="*.csv", full.names=T ,recursive = T)
-    tmp <- lapply(filenames, function (x) read.csv(x, stringsAsFactor=F))
-    return(ldply(tmp, data.frame))
-} 
-
-localisation <- c("sein")
-
-for (file in localisation){
-    tmp <- loadData(file)
-    assign(file, tmp)
-    rm(tmp)
-}   
-
-# Customize the ggplot theme
+# Global ggplot theme customization
 mytheme <- theme_grey(12) + theme(panel.border = element_blank(), 
                                   axis.line = element_line(), 
                                   panel.background = element_blank(),
@@ -27,3 +31,31 @@ mytheme <- theme_grey(12) + theme(panel.border = element_blank(),
                                   panel.grid.major.y = element_line(color="grey90"), 
                                   panel.grid.minor.y = element_blank())
 theme_set(mytheme)
+
+
+################################################################################
+##
+##      Data munching
+##
+################################################################################
+
+# Function to load the SAS outputs and merge them
+loadData <- function(x) {
+        filenames <- list.files(paste0("data/",x), 
+                                pattern="*.csv", 
+                                full.names=T ,
+                                recursive = T)
+        
+        tmp <- lapply(filenames, 
+                      function (x) read.csv(x, stringsAsFactor=F))
+        
+        return(ldply(tmp, data.frame))
+} 
+
+# Create a single dataframe for each type of supported cancer
+for (file in localisation){
+        tmp <- loadData(file)
+        colnames(tmp) <- tolower(colnames(tmp))
+        assign(file, tmp)
+        rm(tmp)
+}   
